326a08853b73a1e8e75857f371aab82c37661e3b
Harsh Lamba <harshlambamanthanitsolutions@gmail.com>
2025-06-26 08:22:21 +0530

    49

--- 34_first_and_last_position_of_element_in_sorted_array/main.go
+++ 34_first_and_last_position_of_element_in_sorted_array/main.go
@@ -7,11 +7,7 @@ func main() {
 
 	var nums []int = []int{1,2,3,4,4,4,6,7,7,7,8} 
 
-
 	fmt.Println(searchRange(nums, 4))
-
-
-
 	
 }
 
@@ -68,3 +64,14 @@ func searchRange(nums []int, target int) []int {
 
 	return []int{min, max}
 }
+
+
+// APPROACH 
+// 		1. first run 1 b search and find the matching element 
+// 				treat the matched element as the center and search both left and right that way the we only get to the context and the  search it around
+// 				flaws: this approach would put it to be O(n) instead of O(log n) because what if the case is [5,5,5,5,5,5,5,5] this the the first mid would match and then the linear search so called(n/2) search has to be done
+// 						doing that O(log n) + O(n) would give the time complexity of O(n) to this algorithm -- failed the constraint  
+
+// 		2. do 2 binary searches 1 finding the min border and the other max border
+//				we have to find the position where nums[mid] is the target and nums[mid - 1] is not the target in finding left border for the right one nums[mid + 1] should not be the target, again the nums[mid] has to be the targe in both the cases
+// 				this way there will be 2 operations O(log n) + O(log n) giving the output of O(log n)
